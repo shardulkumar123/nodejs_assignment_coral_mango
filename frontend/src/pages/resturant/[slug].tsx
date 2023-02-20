92582016f41a189c62b805d2ae3ec85edcae10ac
Shardul Kumar <shardul.kumar@thewitslab.com>
2023-02-20 15:28:49 +0530

    feat: integrated review api

--- frontend/src/pages/resturant/[slug].tsx
+++ frontend/src/pages/resturant/[slug].tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import FoodImage from "public/images/food.jpg";
-
-
 
 type FormData = {
   review: string;
+  // restname : string
 };
-
 
 const ResturantDetails = ({ data }: any) => {
   const [myData, setmyData] = useState<any>([]);
-
+  const [isHide, setIsHide] = useState(true);
+  const [defaultPayload, setdefaultPayload] = useState("");
+  console.log('defaultPayload', defaultPayload)
+  console.log("myData", myData);
   const router = useRouter();
   const { slug } = router.query;
 
@@ -27,40 +26,47 @@ const ResturantDetails = ({ data }: any) => {
     setmyData(effectData);
   }, [data.getAllRestaurant, slug]);
 
-
+  useEffect(() => {
+    setdefaultPayload(myData[0]?.name);
+  }, [myData]);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
-  
-  const onSubmit = handleSubmit( async (data) => {
-    console.log('data', data)
-    // if (data) {
-    //   // console.log(data)
-    //   const res = await fetch("http://localhost:8000/admin-login", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(data),
-    //   });
-    //   console.log('res', res)
-    //  
-    // }
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log("data", data);
+    const { review } = data;
+    if (data) {
+      // console.log(data)
+      const res = await fetch("http://localhost:8000/review", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          review: review,
+          name: defaultPayload,
+        }),
+      });
+    }
   });
-
-
 
   return (
     <div className="bg-slate-500">
       <Navbar />
       <div className="p-5">
         <div className="text-center py-5 text-6xl">{myData[0]?.name}</div>
-        <div className="text-center text-lg font-bold uppercase"><span className="font-bold capitalize">Location :</span>  {myData[0]?.address}</div>
+        <div className="text-center text-lg font-bold uppercase">
+          <span className="font-bold capitalize">Location :</span>{" "}
+          {myData[0]?.address}
+        </div>
         <div className="p-5 text-center">{myData[0]?.desc}</div>
-        <div><span className="font-bold">Pincode :</span> {myData[0]?.pincode}</div>
+        <div>
+          <span className="font-bold">Pincode :</span> {myData[0]?.pincode}
+        </div>
         <div className="">
           <p className="font-bold">Menu : </p>
           {myData[0]?.menu.map((e: any, index: any) => {
@@ -77,9 +83,15 @@ const ResturantDetails = ({ data }: any) => {
                 rows={4}
                 className="w-full px-0 text-sm  bg-gray-800 focus:ring-0 text-gray-400 placeholder-gray-400"
                 placeholder="Write a review..."
-                {...register('review')}
+                {...register("review")}
                 required
               ></textarea>
+              {/* {isHide &&<input
+                id="restname"
+                value={myData[0]?.name}
+                {...register("restname")}
+                required
+              ></input>} */}
             </div>
             <div className="flex items-center justify-between px-3 py-2  border-gray-600">
               <button
@@ -92,7 +104,31 @@ const ResturantDetails = ({ data }: any) => {
           </div>
         </form>
       </div>
-      <Footer/>
+
+      <div className="my-12">
+        <h1 className="text-lg px-5 font-bold">All Reviews :</h1>
+        {myData[0]?.total_reviews.map((e: any, index: any) => (
+          <div key={index} className="my-4 px-5 flex items-center gap-2">
+            <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+              <svg
+                className="absolute w-12 h-12 text-gray-400 -left-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+
+            {e?.review}
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 };
--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -89,7 +89,7 @@ const login = () => {
             </div>
             <button
               type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-blue-800"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
               Sign Up
             </button>
